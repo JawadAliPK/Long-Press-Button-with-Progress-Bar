650d6bea31501a25bcaa4f2e705feb0f167d364d
Jawad Ali <jawad@eddress.co>
2024-03-19 22:36:44 +0100

    Initial Commit

---
+++ LongPressButtonWithProgress/LongPressButtonWithProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongPressButtonWithProgressApp.swift
+//  LongPressButtonWithProgress
+//
+//  Created by jawad ali on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LongPressButtonWithProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
